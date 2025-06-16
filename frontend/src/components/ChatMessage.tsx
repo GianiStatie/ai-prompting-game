0f036fa2438f5e43515544a027cb543e43683bca
GianiStatie <gianistatie@gmail.com>
2025-06-16 18:53:33 +0300

    move ui styles to styles file

--- frontend/src/components/ChatMessage.tsx
+++ frontend/src/components/ChatMessage.tsx
@@ -1,23 +1,10 @@
 import React from 'react';
-import { MessageBubble } from '../styles/Chat';
+import { MessageBubble, Spinner } from '../styles/Chat';
 import { Message } from '../types';
 
 interface ChatMessageProps {
   message: Message;
 }
-
-const Spinner: React.FC = () => (
-  <div style={{
-    display: 'inline-block',
-    width: '16px',
-    height: '16px',
-    border: '2px solid #666',
-    borderTop: '2px solid #fff',
-    borderRadius: '50%',
-    animation: 'spin 1s linear infinite',
-    marginRight: '8px'
-  }} />
-);
 
 export const ChatMessage: React.FC<ChatMessageProps> = ({ message }) => {
   return (
